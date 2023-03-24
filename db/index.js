cdd90e73f7f834238bcf2ccc8683b1ebd3be37f1
Rybchynskyi <alexandr.rybchinskij@gmail.com>
2023-03-24 15:34:24 +0200

    updates

--- db/index.js
+++ db/index.js
@@ -1,8 +1,15 @@
 const env = require("../env.json");
+
+const dbHost = env.LOCAL_DB_HOST || env.GLOBAL_DB_HOST;
+const dbUsername = env.LOCAL_DB_USER || env.LOCAL_DB_USER;
+const dbPassword = env.LOCAL_DB_PASSWORD || env.GLOBAL_DB_PASSWORD;
+const dbDataBase = env.LOCAL_DB_DATABASE || env.GLOBAL_DB_DATABASE;
+const dbPort = env.LOCAL_DB_PORT || env.GLOBAL_DB_PORT;
+
 module.exports = {
-  host     : env.db_host,
-  user     : env.db_user,
-  password : env.db_password,
-  database : env.db_database,
-  port     : env.db_port
+  host     : dbHost,
+  user     : dbUsername,
+  password : dbPassword,
+  database : dbDataBase,
+  port     : dbPort
 }
